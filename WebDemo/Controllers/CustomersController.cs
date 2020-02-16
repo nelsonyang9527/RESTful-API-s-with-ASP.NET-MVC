c09cdb443c78d0c1988b322522923f629517681f
Administrator <35318185+nelsonyang9527@users.noreply.github.com>
2020-02-17 00:51:23 +0800

    * Controller 溝通 Service * Service 繼承 ServiceBase 進行資料庫存取

---
+++ WebDemo/Controllers/CustomersController.cs
@@ -0,0 +1,65 @@
+﻿using Database;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebDemo.Service;
+
+namespace WebDemo.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        CustomersService service = new CustomersService();
+
+        /// <summary>
+        /// 取得客戶資料
+        /// </summary>
+        /// <param name="CustomerID"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public string Get(string CustomerID)
+        {
+            Customers result = service.GetCustomers(CustomerID);
+            return JsonConvert.SerializeObject(result);
+        }
+
+        /// <summary>
+        /// 新增客戶資料
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public string Post(Customers value)
+        {
+            bool result = service.AddCustomers(value);
+            return JsonConvert.SerializeObject(result);
+        }
+
+        /// <summary>
+        /// 修改客戶資料
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public string Put(Customers value)
+        {
+            bool result = service.UpdateCustomers(value);
+            return JsonConvert.SerializeObject(result);
+        }
+
+        /// <summary>
+        /// 刪除客戶資料
+        /// </summary>
+        /// <param name="CustomerID"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public string Delete(string CustomerID)
+        {
+            bool result = service.DeleteCustomers(CustomerID);
+            return JsonConvert.SerializeObject(result);
+        }
+    }
+}
---
+++ WebDemo/Service/CustomersService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Database;
+using EFDAL;
+
+namespace WebDemo.Service
+{
+    public class CustomersService : ServiceBase
+    {
+        /// <summary>
+        /// 取得客戶資料
+        /// </summary>
+        /// <param name="CustomerID">客戶ID</param>
+        /// <returns></returns>
+        public Customers GetCustomers(string CustomerID)
+        {
+            IQueryable<Customers> CustomersBase = base.Lookup<Customers>(x => x.CustomerID == CustomerID);
+            return (CustomersBase.Any()) ? CustomersBase.FirstOrDefault() : null;
+        }
+
+        /// <summary>
+        /// 新增客戶資料
+        /// </summary>
+        /// <param name="createCustomers">新增資料</param>
+        /// <returns></returns>
+        public bool AddCustomers(Customers createCustomers)
+        {
+            createCustomers.CustomerID = Guid.NewGuid().ToString("N").Substring(0, 5);
+            return base.Insert<Customers>(createCustomers);
+        }
+
+        /// <summary>
+        /// 修改客戶資料
+        /// </summary>
+        /// <param name="updateCustomers">修改資料</param>
+        /// <returns></returns>
+        public bool UpdateCustomers(Customers updateCustomers)
+        {
+            IQueryable<Customers> CustomersBase = base.Lookup<Customers>(x => x.CustomerID == updateCustomers.CustomerID);
+            if(false == CustomersBase.Any()) { return false; }
+            Customers UpdateData = CustomersBase.FirstOrDefault();
+            UpdateData.ContactName = updateCustomers.ContactName;
+            return base.Update<Customers>(UpdateData);
+        }
+
+        /// <summary>
+        /// 刪除客戶資料
+        /// </summary>
+        /// <param name="CustomerID">客戶ID</param>
+        /// <returns></returns>
+        public bool DeleteCustomers(string CustomerID)
+        {
+            Customers deleteData = GetCustomers(CustomerID);
+            if (null == deleteData) { return false; }
+            return base.Delete<Customers>(deleteData);
+        }
+    }
+}
