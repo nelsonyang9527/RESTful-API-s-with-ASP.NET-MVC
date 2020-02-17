2aade7c980868c37df5b11d5ef6f64421aba951e
Administrator <35318185+nelsonyang9527@users.noreply.github.com>
2020-02-18 00:42:36 +0800

    加入單元測試專案, 並撰寫簡易CRUD單元測試

---
+++ WebDemo.Tests/Controllers/CustomersControllerTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Database;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebDemo.Service;
+
+namespace WebDemo.Tests
+{
+    [TestClass]
+    public class CustomersControllerTest
+    {
+        CustomersService service = new CustomersService();
+
+        /// <summary>
+        /// 主測試程序 CRUD
+        /// </summary>
+        [TestMethod]
+        public void main()
+        {
+            Customers data = new Customers();
+            data.CustomerID = Guid.NewGuid().ToString("N").Substring(0, 5);
+            data.ContactName = "測試人";
+            data.CompanyName = "測試公司";
+            // 增
+            Add(data);
+            // 改
+            data.ContactName = "測試人(改)";
+            Update(data);
+            // 查
+            Get(data);
+            // 刪
+            Delete(data);
+        }
+
+        /// <summary>
+        /// 增
+        /// </summary>
+        /// <param name="data"></param>
+        private void Add(Customers data)
+        {
+            bool result = service.AddCustomers(data);
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// 改
+        /// </summary>
+        /// <param name="data"></param>
+        private void Update(Customers data)
+        {
+            bool result = service.UpdateCustomers(data);
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// 查
+        /// </summary>
+        /// <param name="data"></param>
+        private void Get(Customers data)
+        {
+            Customers result = service.GetCustomers(data.CustomerID);
+            Assert.IsInstanceOfType(result, typeof(Customers));
+        }
+
+        /// <summary>
+        /// 刪
+        /// </summary>
+        /// <param name="data"></param>
+        private void Delete(Customers data)
+        {
+            bool result = service.DeleteCustomers(data.CustomerID);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ WebDemo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebDemo.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebDemo.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89ad1e75-8eef-4c21-8c15-9cad6487dfbd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
